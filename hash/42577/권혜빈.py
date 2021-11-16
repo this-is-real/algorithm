ad9b55b8d5e392c902e42af22559f737b2b66158
hbinn <50905732+hbinn@users.noreply.github.com>
2021-11-16 15:43:41 +0900

    Update 권혜빈.py

--- hash/42577/권혜빈.py
+++ hash/42577/권혜빈.py
@@ -1,3 +1,13 @@
+# def solution(phone_book):
+#     m = min(phone_book)
+#     phone_book.remove(m)
+    
+#     for i in phone_book:
+#         if m == i[:len(m)]:
+#             return False
+    
+#     return True
+
 def solution(phone_book):
     answer = True
     phone_book.sort()
