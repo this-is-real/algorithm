9b1013965656f2fcd93ff228e628adf47ab04a21
zeus0007 <wpdn79@likelion.org>
2021-11-25 15:30:53 +0900

    김제우

---
+++ exhaustive_search/카펫(42842)/김제우.py
@@ -0,0 +1,19 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    
+    #공배수 찾기 -> frame 만들기
+    frames = []
+    for i in range(3,total//2):
+        if total % i == 0 and i >= total//i:
+            frames.append((i,total//i)) 
+            
+    answer = []
+    for width, height in frames:
+        brown_count = 0
+        for i in range(width//2):
+            brown_count += 2*(width-2*i) + 2*(height-2*i) - 4
+            if brown_count == brown and width*height-brown_count == yellow:
+                answer.append(width)
+                answer.append(height)
+    
+    return answer
