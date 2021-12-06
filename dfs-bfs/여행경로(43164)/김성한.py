ceef5e8c059b53830e8a66423e0c1f5d3fa7162b
zgotter <shkim4738@gmail.com>
2021-12-06 12:29:53 +0900

    DFS/BFS - 여행경로 (김성한)

---
+++ dfs-bfs/여행경로(43164)/김성한.py
@@ -0,0 +1,24 @@
+from collections import defaultdict, deque
+
+def get_graph(tickets):
+    graph = defaultdict(list)
+    for s_city, d_city in tickets:
+        graph[s_city].append(d_city)
+    graph = {key: sorted(value) for key, value in graph.items()}
+    return graph
+
+def solution(tickets):
+    graph = get_graph(tickets)
+    
+    stack = deque(["ICN"])
+    answer = []
+    while stack:
+        city = stack[-1]
+        if city not in graph.keys() or len(graph[city]) == 0:
+            answer.append(stack.pop())
+        else:
+            stack.append(graph[city].pop(0))
+    
+    answer = answer[::-1]
+    
+    return answer
