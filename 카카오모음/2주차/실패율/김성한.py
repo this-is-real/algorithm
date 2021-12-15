a50fa00bb3de616d6b75e38beab97580aa276888
zgotter <shkim4738@gmail.com>
2021-12-15 23:59:26 +0900

    실패율 (김성한)

---
+++ 카카오모음/2주차/실패율/김성한.py
@@ -0,0 +1,9 @@
+def solution(N, stages):
+    answer = []
+    for i in range(1, N+1):
+        a = [s for s in stages if s >= i]
+        b = [s for s in stages if s == i]
+        answer.append((i, 0 if not a else len(b) / len(a)))
+    answer = sorted(answer, key=lambda x: -x[1])
+    answer = [ans for ans, _ in answer]
+    return answer
