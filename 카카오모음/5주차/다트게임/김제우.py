1ae2860653dbc87d82b8e62f23aa73cebf7103d6
zeus0007 <wpdn79@likelion.org>
2022-01-04 14:59:53 +0900

    김제우

---
+++ 카카오모음/5주차/다트게임/김제우.py
@@ -0,0 +1,40 @@
+import re
+def solution(dartResult):
+    dart_result = re.findall(r"[0-9]{1,2}[SDT*#]{1,2}",dartResult)
+    splited_darts = []
+    for dart in dart_result :
+        number = re.findall(r"[0-9]{1,2}", dart)[0]
+        area = re.findall(r"[SDT]",dart)[0]
+        option = re.findall(r"[*#]",dart)
+        if option :
+            option = option[0]
+        else :
+            option = ""
+        splited_darts.append([number,area,option])
+    
+    score = 0
+    result = []
+    for i,item in enumerate(splited_darts):
+        temp_score = 0
+        temp_score += int(item[0])
+        if item[1] == "S":
+            temp_score = temp_score**1
+        elif item[1] == "D":
+            temp_score = temp_score**2
+        elif item[1] == "T":
+            temp_score = temp_score**3
+        if item[2] == "*":
+            if i == 0:
+                temp_score *= 2
+            else :
+                temp_score *= 2
+                past_score = result.pop()
+                result.append(past_score * 2)
+        elif item[2] == "#":
+            temp_score = -temp_score
+        
+        result.append(temp_score)    
+            
+                
+    answer = sum(result)
+    return answer
