b0b6e66aad352de32f79c901c5206b0b1502d3ce
zgotter <shkim4738@gmail.com>
2022-01-30 15:08:18 +0900

    카카오 - level3 - 징검다리 건너기 (김성한)

---
+++ 카카오모음/8주차/징검다리 건너기/김성한.py
@@ -0,0 +1,17 @@
+def solution(stones, k):
+    start, end = 0, max(stones)
+    while start <= end:
+        mid = (start + end) // 2
+        count = 0
+        for stone in stones:
+            if count == k:
+                break
+            if stone - mid <= 0:
+                count += 1
+            else:
+                count = 0
+        if count < k:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
